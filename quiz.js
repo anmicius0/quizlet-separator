ed317220a261ff4328e54b98ea0f8a370b0d376d
cg <cgallen@pm.me>
2019-02-22 16:22:23 +0800

    Finish js part

--- quiz.js
+++ quiz.js
@@ -1,3 +1,49 @@
-function separate() {
+
+function validation() {
+    /* 
+        get the form input and set config value
+    */ 
+
+    let input = document.getElementById("textarea").value;
+    var separator = document.forms["separator-form"]["separator"].value;
     
+    if ( separator === "") {
+       separator = document.forms["separator-form"]["custom-separator"].value;
+    };
+
+    separate(input, separator);
+}
+
+function separate(text, separator) {
+    /*
+        args:
+            text: the user's input strgin 
+            separator: separator
+    */
+
+    let lines = [];
+    let words = [];
+    let eng_words = [];
+
+    lines = text.split("\n");
+    
+    for (let i=0; i<lines.length; i++) {
+        let temp = lines[i].split('\t');
+
+        temp.forEach( word => {
+            words.push(word + '\t')
+        });
+    };
+
+    for (let i=0; i<words.length; i+=2) {
+        eng_words.push( words[i]);
+    };
+
+    show_result(eng_words);
+}
+
+function show_result(eng_words) {
+
+    document.getElementById("textarea").value = eng_words;
+    document.getElementsByTagName("button")[0].setAttribute();
 }
