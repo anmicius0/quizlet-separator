9e6a063dba9cef445f7d3b6fa11c7280a0bd5e57
cg <cgallen@pm.me>
2019-02-23 12:50:00 +0800

    add sweat alert

--- quiz.js
+++ quiz.js
@@ -39,5 +39,20 @@ function copy() {
     // Hide "submit" button and show "copy" button
     document.getElementById("submit").style.display = "none";
     document.getElementById("copy").style.display = "block";
+    document.getElementById("copy-again").style.display = "block";
 
+    // beta
+    // swal
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
+      
+      Toast.fire({
+        type: 'success',
+        title: 'Copied',
+        text: 'you can now go to vocabulary.com'
+      })
 }
